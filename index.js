b34cfbc1a619a7cd5a901f3731a9199336de0288
LincKim <lincolnkimutai@gmail.com>
2022-11-26 09:06:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(number, number2){
+    // let num1 = 2000
+    // let num2 = 100
+    return 2000/100
+    
+}
+divide()
+
+const square = x => x * x;
+
+const add = (a, b) => {
+       
+    return a + b
+}
